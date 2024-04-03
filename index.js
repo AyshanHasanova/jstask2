96efb09488f47d803a56f0a1faed60c6636b6fb2
unknown <ahsnova557@gmail.com>
2024-04-04 00:40:10 +0400

    task2 added

---
+++ index.js
@@ -0,0 +1,37 @@
+  let eded= parseInt(prompt("eded daxil edin"));
+  let count=0
+  var hal =0
+  for (let index = 1; index <= eded; index++) {
+    if(eded %index==0){
+        count++
+        
+   
+    }
+  }
+    
+    if(count>2){
+        console.log("eded murekkebdir")
+       
+    }
+    else {
+        console.log("eded sadedir")
+        
+    }
+    
+task2
+
+   var cumle ;
+   var count = 0
+   function getcount(cumle){
+    var parcala =cumle.split (" ");
+    count= parcala.length
+    console.log(count);
+
+   }
+   getcount("salam sabahnz xeyir")
+
+ 
+
+
+
+
